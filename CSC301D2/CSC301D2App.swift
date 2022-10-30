ebcb1df6ecad4c687e3b2e8880dfbd4bdf8cc110
natyho <natalie200078@gmail.com>
2022-10-30 12:39:40 -0400

    Initial Commit

---
+++ CSC301D2/CSC301D2App.swift
@@ -0,0 +1,17 @@
+//
+//  CSC301D2App.swift
+//  CSC301D2
+//
+//  Created by Natalie Ho on 2022-10-30.
+//
+
+import SwiftUI
+
+@main
+struct CSC301D2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSC301D2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSC301D2
+//
+//  Created by Natalie Ho on 2022-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
